85b2f9c0146f4b4e464d286bb91c530059947db8
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-04-30 01:12:14 +0200

    Implemented json deserialize from stream

--- src/RedHttpServer/Extensions/NewtonsoftJsonConverter.cs
+++ src/RedHttpServer/Extensions/NewtonsoftJsonConverter.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using Newtonsoft.Json;
 using Red.Interfaces;
 
@@ -8,14 +9,27 @@ namespace Red.Extensions
     /// </summary>
     internal sealed class NewtonsoftJsonConverter : IJsonConverter, IRedExtension
     {
+        /// <inheritdoc />
         public string Serialize<T>(T obj)
         {
             return JsonConvert.SerializeObject(obj);
         }
 
+        /// <inheritdoc />
         public T Deserialize<T>(string jsonData)
         {
             return JsonConvert.DeserializeObject<T>(jsonData);
+        }
+        
+        /// <inheritdoc />
+        public T Deserialize<T>(Stream jsonStream)
+        {
+            using (var reader = new StreamReader(jsonStream))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                var serializer = new JsonSerializer();
+                return serializer.Deserialize<T>(jsonReader);
+            }
         }
 
         public void Initialize(RedHttpServer server)
