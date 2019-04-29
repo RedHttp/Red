84fe150555cbbe9937413102a496df830a9b94f3
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-04-30 01:11:17 +0200

    Changed signature of Parse function

--- src/RedHttpServer/Interfaces/IBodyParser.cs
+++ src/RedHttpServer/Interfaces/IBodyParser.cs
@@ -8,11 +8,19 @@ namespace Red.Interfaces
     public interface IBodyParser
     {
         /// <summary>
-        /// 
+        /// Parse the request body stream into a string
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        Task<string> Parse(Request request);
+        
+        
+        /// <summary>
+        /// Parse the request body stream into an object of a given type
         /// </summary>
         /// <param name="request"></param>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
-        Task<T> Parse<T>(Request request);
+        T Parse<T>(Request request);
     }
 }
