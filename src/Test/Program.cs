ce37e3c25f121ad43e6daa8934ff0de986e36e14
rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-11-03 15:17:08 +0100

    Update demo project

--- src/Test/Program.cs
+++ src/Test/Program.cs
@@ -7,6 +7,12 @@ using Red;
 
 namespace Test
 {
+    
+    public class TestObject
+    {
+        public string Prop1 { get; set; }
+        public int Prop2 { get; set; }
+    }
     class MySess
     {
         public string Name { get; set; }
@@ -22,6 +28,8 @@ namespace Test
         {
             return HandlerType.Final;
         }
+
+        
         static async Task Main(string[] args)
         {
             var server = new RedHttpServer(5001);
@@ -31,8 +39,17 @@ namespace Test
             server.Get("/webm", (req, res) => res.SendFile("./Big_Buck_Bunny_alt.webm"));
             server.Get("/files/*", Utils.SendFiles("public/files"));
             
+            var testObj = new TestObject{Prop1 = "Hello", Prop2 = 42};
+            
+            server.Get("/json", (req, res) => res.SendJson(testObj));
+            server.Get("/xml", (req, res) => res.SendXml(testObj));
+            
             TestRoutes.Register(server.CreateRouter("/test"));
 
+            server.OnHandlerException += (e, sender) =>
+            {
+                Console.WriteLine(e);
+            };
             
             server.WebSocket("/echo", Auth, async (req, res, wsd) =>
             {
